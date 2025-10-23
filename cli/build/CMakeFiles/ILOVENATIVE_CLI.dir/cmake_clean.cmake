c52e5cc6007f0ece0c000a09f603cfeaa31f64e1
Sarthak <aniki123.ai@gmail.com>
2025-10-23 18:35:19 +0530

    --build

---
+++ cli/build/CMakeFiles/ILOVENATIVE_CLI.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ILOVENATIVE_CLI.dir/command_handler.cpp.o"
+  "CMakeFiles/ILOVENATIVE_CLI.dir/command_handler.cpp.o.d"
+  "CMakeFiles/ILOVENATIVE_CLI.dir/main.cpp.o"
+  "CMakeFiles/ILOVENATIVE_CLI.dir/main.cpp.o.d"
+  "bin/ILOVENATIVE_CLI"
+  "bin/ILOVENATIVE_CLI.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ILOVENATIVE_CLI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cli/build/CMakeFiles/ILOVENATIVE_CLI.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ILOVENATIVE_CLI.
+# This may be replaced when dependencies are built.
